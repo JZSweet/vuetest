f93a04b0bf0fb4bdb28bdd5efd4ecdafd18599aa
JZ <73402906+JZSweet@users.noreply.github.com>
2023-08-07 20:34:39 -0700

    add components to file

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -4,8 +4,16 @@
       {{ global[0].title }}
     </h1>
     <img :src="$mediumImgURL(global[0].previewImage?.asset._ref)" class='test-border'/>
-      
- 
+      <div class="grid grid-cols-3">
+           <div v-for="project in projects" class="card">
+          <h2>
+            {{ project.title }}
+          </h2>
+          <img :src="$smallImgURL(project.mainImage)"/>
+          <div v-html="$portableTextToHTML(project.description)"> </div> 
+          </div>
+      </div>
+ <Header/>
   </div>
 </template>
 
